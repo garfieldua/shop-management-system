9e85acfcef7cad7e9c2b2ee43e2e193a8352a46e
garfieldua <andrey.davydenko@gmail.com>
2015-02-17 10:51:50 +0200

    #4 add database connector class

---
+++ src/com/naukma/shop/database/MySQLDatabase.java
@@ -0,0 +1,54 @@
+package com.naukma.shop.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public final class MySQLDatabase {
+	
+	private Connection con;
+	private static MySQLDatabase instance = null;
+	
+	public static final String URL = "";
+	public static final String USER = "";
+	public static final String PASSWORD = "";
+	public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+
+	private MySQLDatabase() {
+		try {
+			Class.forName(DRIVER_CLASS);
+			try {
+				con = DriverManager.getConnection(URL, USER, PASSWORD);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static MySQLDatabase getInstance() {
+		if (instance == null) {
+			instance = new MySQLDatabase();
+		}
+		return instance;
+	}
+
+	public ResultSet execute(String query) {
+		try {
+			Statement s = con.createStatement();
+			
+			s.execute(query);
+			ResultSet rs = s.getResultSet();
+
+			return rs;
+		}
+
+		catch (SQLException e) {
+			System.out.println("Error: " + e);
+		}
+		return null;
+	}
+}
