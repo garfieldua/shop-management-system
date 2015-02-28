3d45aac9a0de2c8b84bf6a7d0fd76cadf41ae52a
Дученчук Володимир <dvbgamesua@gmail.com>
2015-02-28 19:24:42 +0200

    #18 seller views buttons logic

--- src/com/naukma/shop/controller/SellerController.java
+++ src/com/naukma/shop/controller/SellerController.java
@@ -1,19 +1,57 @@
 package com.naukma.shop.controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTable;
+
+import com.naukma.shop.view.AllItemsView;
 import com.naukma.shop.view.SellProductView;
-import com.naukma.shop.view.StorekeeperView;
+//import com.naukma.shop.view.StorekeeperView;
 
 public class SellerController extends AbstractController {
-	private SellProductView view; 
+	private SellProductView sellProductView; 
+	private AllItemsView allItemsView;
 
 	public SellerController() {
-		view = new SellProductView();
+		sellProductView = new SellProductView();
+		allItemsView = new AllItemsView();
+		
+		// proceed to all items view
+		sellProductView.getBtnAllItems().addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e)
+	        {
+	        	MainController.getInstance().setPanel(allItemsView);
+	        }
+		});
+		fillSellerView();
+		
+		// back button from All Items
+		allItemsView.getBtnBackButton().addActionListener(new ActionListener() {
+	        public void actionPerformed(ActionEvent e)
+	        {
+	        	MainController.getInstance().setPanel(sellProductView);
+	        }
+		});
+		fillAllItemsView();
+	
 	}
 	
 	@Override
 	public void assignToMainContrainer() {
-		MainController.getInstance().setPanel(view);
-		MainController.getInstance().addLogOutButtonListener(view);
+		MainController.getInstance().setPanel(sellProductView);
+		MainController.getInstance().addLogOutButtonListener(sellProductView);
+	}
+	
+	public void fillAllItemsView() {
+	 
+	    
+	    //JTable table = allItemsView.getTable();
+	    //table = //new JTable(headers, data);
+	}
+	
+	public void fillSellerView() {
+		
 	}
 	
 }
--- src/com/naukma/shop/view/AllItemsView.java
+++ src/com/naukma/shop/view/AllItemsView.java
@@ -1,14 +1,21 @@
 package com.naukma.shop.view;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
+import javax.swing.JButton;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AllItemsView extends PanelWithLogOut {
 	private JTable table;
 	private JLabel lblAllItems;
 	private JScrollPane scrollPaneAllItems;
+	private JButton btnBackButton;
+	
 	public AllItemsView() {
 		getBtnLogOut().setLocation(500, 11);
 		
@@ -18,11 +25,31 @@ public class AllItemsView extends PanelWithLogOut {
 		add(lblAllItems);
 		
 		scrollPaneAllItems = new JScrollPane();
-		scrollPaneAllItems.setBounds(50, 100, 500, 350);
+		scrollPaneAllItems.setBounds(50, 100, 500, 312);
 		add(scrollPaneAllItems);
-		
+		    
 		table = new JTable();
 		scrollPaneAllItems.setViewportView(table);
+		
+		btnBackButton = new JButton("Back");
+		btnBackButton.setBounds(50, 431, 117, 29);
+		add(btnBackButton);
+	}
+	
+	public JTable getTable() {
+		return table;
 	}
 
+	public JLabel getLblAllItems() {
+		return lblAllItems;
+	}
+
+	public JScrollPane getScrollPaneAllItems() {
+		return scrollPaneAllItems;
+	}
+
+	public JButton getBtnBackButton() {
+		return btnBackButton;
+	}
+	
 }
